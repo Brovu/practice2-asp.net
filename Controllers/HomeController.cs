b6d656e456d36d27892b63997b7413c75c8ccd16
Brovu <roy77203@gmail.com>
2023-06-28 21:46:58 +0700

    done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice2.Models;
+
+namespace Practice2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string filter, int? id)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+            List<News> listNews = db.News.Where(m => m.Title.ToLower().Contains(filter.ToLower()) == true || m.Date.ToString().Contains(filter) & (m.idType == id | id == null)).ToList();
+
+            /*List<News> listNews = (from m in db.News
+                                   join typeNews in db.TypeNews on m.idType equals typeNews.ID
+                                   where (m.Title.ToLower().Contains(filter.ToLower()) == true || m.Date.ToString().Contains(filter) & (m.idType == id | id == null))
+                                   select m
+                                 ).ToList();*/
+            if (!string.IsNullOrEmpty(filter) && listNews.Count == 0)
+            {
+                ViewBag.Message = "Không tìm thấy kết quả!";
+            }
+
+            if (listNews.Count == 0)
+            {
+                return View(db.News.ToList());
+            }
+
+            return View(listNews);
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+
+        public ActionResult AddNews()
+        {
+            return View(new News());
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+
+        public ActionResult AddNews(News model)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+
+            db.News.Add(model);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditNews(string id)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+            var model = db.News.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+
+        public ActionResult EditNews(News model)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+
+            var updateNews = db.News.Find(model.ID);
+
+            updateNews.isDisplay = model.isDisplay;
+            updateNews.Title = model.Title;
+            updateNews.ID = model.ID;
+            updateNews.ImagePath = model.ImagePath;
+            updateNews.Date = model.Date;
+            updateNews.idType = model.idType;
+            updateNews.Description = model.Description;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteNews(string id)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+            var model = db.News.Find(id);
+            db.News.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/TypeNewsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice2.Models;
+
+
+namespace Practice2.Controllers
+{
+    public class TypeNewsController : Controller
+    {
+        public ActionResult TypeList()
+        {
+            return View(new ManageNewsEntities().TypeNews.ToList());
+        }
+
+        public ActionResult AddType()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AddType(TypeNew model)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+
+            if(string.IsNullOrEmpty(model.typeName) == true)
+            {
+                ModelState.AddModelError("", "Chưa nhập");
+                return View(model);
+            }
+
+            try
+            {
+                db.TypeNews.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("TypeList");
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(model);
+            }
+
+        }
+
+        public ActionResult EditType(int id)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+            var model = db.TypeNews.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditType(TypeNew model) {
+            
+
+            if (string.IsNullOrEmpty(model.typeName) == true)
+            {
+                ModelState.AddModelError("", "Chưa nhập");
+                return View(model);
+            }
+
+            ManageNewsEntities db = new ManageNewsEntities();
+            var updateType = db.TypeNews.Find(model.ID);
+            try
+            {
+                updateType.typeName = model.typeName;
+                db.SaveChanges();
+                return RedirectToAction("TypeList");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(model);
+            }
+        }
+        
+        public ActionResult DeleteType(int id)
+        {
+            ManageNewsEntities db = new ManageNewsEntities();
+            var model = db.TypeNews.Find(id);
+
+            db.TypeNews.Remove(model);
+            db.SaveChanges();
+
+            return RedirectToAction("TypeList"); 
+        }
+    }
+}
